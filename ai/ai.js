a3256a9fe79804b4b7bdefcedde848dbec91dcce
Курилев Владислав Александрович <Kurilev.VA@ipprt.ru>
2025-04-04 15:29:59 +0300

    add ai telegram bot

---
+++ ai/ai.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import 'dotenv/config'
+
+const API_KEY = process.env.API_KEY;
+const API_URL = process.env.API_URL;
+
+const sendRequestToMistralAi = async (text) => {
+    try {
+        const response = await axios.post(
+            API_URL,
+            {
+                model: 'mistral-tiny',
+                messages: [
+                    {
+                        role: 'system',
+                        content: 'Ты искуственный интелект в телеграм канале. Отвечай всегда на русском языке и твои ответы должны выглядить как пост в телеграм канале с правильной версткой'
+                    },
+                    {
+                        role: 'user',
+                        content: text
+                    }
+                ],
+                temperature: 0.7,
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${API_KEY}`
+                }
+            }
+        )
+        return response.data.choices[0].message.content
+    } catch (error) {
+        console.error('Ошибка при запросе', error.response)
+        return 'Упс, произошла ошибка, попробуйте позже'
+    }
+}
+
+export default sendRequestToMistralAi
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { gameOprions, againOptions } from './options.js';
+import sendRequestToMistralAi from './ai/ai.js';
 import 'dotenv/config'
 
 const token = process.env.DB_TOKEN;
+const chanelId = process.env.CHANEL_ID;
 const bot = new TelegramBot(token, { polling: true })
+
+process.on('unhandledRejection', (err) => {
+    console.error('UNHANDLED REJECTION', err);
+    // Не падаем, а логируем и продолжаем
+});
+
+process.on('uncaughtException', (err) => {
+    console.error('UNCAUGHT EXCEPTION', err);
+    // Критичные ошибки: завершаем процесс (система оркестрации перезапустит)
+    if (isCriticalError(err)) process.exit(1);
+});
 
 const chats = {}
 
@@ -14,44 +27,60 @@ const startGame = async (chatid) => {
     await bot.sendMessage(chatid, `Отгадай какую`, gameOprions);
 }
 
-const start = () => {
-    bot.setMyCommands([
-        { command: '/start', description: 'Старт проекта' },
-        { command: '/info', description: 'Информация о проекте' },
-        { command: '/game', description: 'Поиграем немного' }
-    ])
-    bot.on('message', async (msg) => {
-        const text = msg.text;
-        const chatid = msg.chat.id
-        if (text === '/start') {
-            await bot.sendMessage(chatid, `Добро пожаловать`)
-            return bot.sendSticker(chatid, 'https://tlgrm.ru/_/stickers/e65/38d/e6538d88-ed55-39d9-a67f-ad97feea9c01/1.webp')
-        }
-        if (text === '/info') {
-            return bot.sendMessage(chatid, `Тебя зовут ${msg.chat.username}`)
-        }
-        if (text === '/game') {
-            return startGame(chatid)
-        }
 
-        return bot.sendMessage(chatid, 'Не понял')
-    })
-
-    bot.on('callback_query', async (msg) => {
-        const data = msg.data;
-        const chatid = msg.message.chat.id;
-
-        if (msg.data === '/again') {
-            return startGame(chatid)
-        };
-
-        await bot.sendMessage(chatid, `Ты выбрал цифру ${data}`)
-        if (data === JSON.stringify(chats[chatid])) {
-            bot.sendMessage(chatid, 'Ты отгадал', againOptions)
-        } else {
-            bot.sendMessage(chatid, 'Ты не отгадал, пробуй еще', againOptions)
-        }
-    })
+const sendMessageToChannel = async (mes, selfId) => {
+    const answer = await sendRequestToMistralAi(mes);
+    // отправка сообщения в канал
+    await bot.sendMessage(chanelId, answer);
+    await bot.sendMessage(selfId, answer);
 }
 
-start()
+const start = async () => {
+    try {
+        bot.setMyCommands([
+            { command: '/start', description: 'Старт проекта' },
+            { command: '/info', description: 'Информация о проекте' },
+            { command: '/game', description: 'Поиграем немного' }
+        ])
+        bot.on('message', async (msg) => {
+            const text = msg.text;
+            const chatid = msg.chat.id
+            if (text === '/start') {
+                await bot.sendMessage(chatid, `Добро пожаловать`);
+                sendMessageToChannel('hello');
+                return bot.sendSticker(chatid, 'https://tlgrm.ru/_/stickers/e65/38d/e6538d88-ed55-39d9-a67f-ad97feea9c01/1.webp')
+            }
+            if (text === '/info') {
+                return bot.sendMessage(chatid, `Тебя зовут ${msg.chat.username}`)
+            }
+            if (text === '/game') {
+                return startGame(chatid)
+            }
+
+            return sendMessageToChannel(msg.text, chatid);
+        })
+
+        bot.on('callback_query', async (msg) => {
+            const data = msg.data;
+            const chatid = msg.message.chat.id;
+
+            if (msg.data === '/again') {
+                return startGame(chatid)
+            };
+
+            await bot.sendMessage(chatid, `Ты выбрал цифру ${data}`)
+            if (data === JSON.stringify(chats[chatid])) {
+                bot.sendMessage(chatid, 'Ты отгадал', againOptions)
+            } else {
+                bot.sendMessage(chatid, 'Ты не отгадал, пробуй еще', againOptions)
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        start()
+    }
+}
+
+start().catch(error => {
+    console.log(error)
+})
